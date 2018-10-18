8bf6d2a3b29c598b8547b000552cea94d605232d
tamanous <tamanous@gmail.com>
2018-10-18 00:29:43 -0700

    added main file, completed q1 abd q1 of titanic example project

---
+++ projects/titanic_survival_exploration/mainTitanic.py
@@ -0,0 +1,83 @@
+# Import libraries necessary for this project
+import numpy as np
+import pandas as pd
+from IPython.display import display # Allows the use of display() for DataFrames
+
+# Import supplementary visualizations code visuals.py
+import visuals as vs
+
+# Pretty display for notebooks
+#matplotlib inline
+
+# Load the dataset
+in_file = 'titanic_data.csv'
+full_data = pd.read_csv(in_file)
+
+# Print the first few entries of the RMS Titanic data
+display(full_data.head())
+
+# Store the 'Survived' feature in a new variable and remove it from the dataset
+outcomes = full_data['Survived']
+data = full_data.drop('Survived', axis = 1)
+
+# Show the new dataset with 'Survived' removed
+display(data.head())
+
+def accuracy_score(truth, pred):
+    """ Returns accuracy score for input truth and predictions. """
+    
+    # Ensure that the number of predictions matches number of outcomes
+    if len(truth) == len(pred): 
+        
+        # Calculate and return the accuracy as a percent
+        return "Predictions have an accuracy of {:.2f}%.".format((truth == pred).mean()*100)
+    
+    else:
+        return "Number of predictions does not match number of outcomes!"
+    
+# Test the 'accuracy_score' function
+predictions = pd.Series(np.ones(5, dtype = int)) # grab the first 5 predictions
+print(accuracy_score(outcomes[:5], predictions)) # only do the first 5 outcomes
+
+def predictions_0(data):
+    """ Model with no features. Always predicts a passenger did not survive. """
+
+    predictions = []
+    for _, passenger in data.iterrows():
+        
+        # Predict the survival of 'passenger'
+        predictions.append(0)
+    
+    # Return our predictions
+    return pd.Series(predictions)
+
+# Make the predictions
+predictions = predictions_0(data)
+
+print(accuracy_score(outcomes, predictions)) # this time check all teh data not just 5
+
+vs.survival_stats(data, outcomes, 'Sex')
+
+def predictions_1(data):
+    """ Model with one feature: 
+            - Predict a passenger survived if they are female. """
+    
+    predictions = []
+    for _, passenger in data.iterrows():
+        
+        # Remove the 'pass' statement below 
+        # and write your prediction conditions here
+        #pass
+        if passenger['Sex'] == 'male':
+            predictions.append(0)
+        else: # female
+            predictions.append(1)
+    
+    # Return our predictions
+    return pd.Series(predictions)
+
+# Make the predictions
+predictions = predictions_1(data)
+
+# Display results with the new prediction criteria
+print(accuracy_score(outcomes, predictions))
