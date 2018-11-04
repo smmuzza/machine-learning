3e63f8607846380c3f9a711315723fb0d445b932
tamanous <tamanous@gmail.com>
2018-11-03 23:49:58 -0700

    started lesson 13

---
+++ lessons/13/lesson13.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Nov  3 23:14:16 2018
+
+@author: shane
+
+DECISION TREES
+
+Cutting the data with multiple lines at each branch in the tree.
+
+ENTROPY
+
+The more homogeneous a set, the less entropy
+
+The less deltas or possible configurations, the less entropy
+
+Low knowledge = high entropy and visa vera
+
+
+"""
+
